50f20976d043252d057a49c1c67e0abbd7fe17ca
Alexey Ivushkin <ivushkinalex@gmail.com>
2021-07-01 04:06:37 +0300

    Change code with advice from @Darksonn References tokio-rs/tokio#3905

--- src/lib.rs
+++ src/lib.rs
@@ -1,29 +1,18 @@
 use std::io::Result;
 use std::pin::Pin;
-use std::task::{Context, Poll, Waker};
-use tokio::io::{AsyncRead, AsyncWrite, ReadBuf};
+use std::task::{Context, Poll};
+use tokio::io::AsyncWrite;
 
 pub struct Gateway {
     data: Vec<u8>,
-    pos: usize,
     is_closed: bool,
-    waker: Option<Waker>,
 }
 
 impl Gateway {
     pub fn new() -> Self {
         Gateway {
             data: Vec::new(),
-            pos: 0,
             is_closed: false,
-            waker: None,
-        }
-    }
-
-    fn close_write(&mut self) {
-        self.is_closed = true;
-        if let Some(waker) = self.waker.take() {
-            waker.wake();
         }
     }
 
@@ -33,32 +22,27 @@ impl Gateway {
             false => None,
         }
     }
-}
 
-impl AsyncRead for Gateway {
-    fn poll_read(
-        mut self: Pin<&mut Self>,
-        cx: &mut Context<'_>,
-        buf: &mut ReadBuf<'_>,
-    ) -> Poll<Result<()>> {
-        match (self.is_closed, self.pos < self.data.len()) {
-            (true, true) => {
-                let len = (self.data.len() - self.pos).min(buf.remaining());
-                buf.put_slice(&self.data[self.pos..self.pos + len]);
-                self.pos += len;
-                Poll::Ready(Ok(()))
-            }
-            (true, false) => Poll::Ready(Ok(())),
-            (false, _) => {
-                self.waker = Some(cx.waker().clone());
-                Poll::Pending
-            }
+    pub fn as_mut(&mut self) -> Option<&mut Vec<u8>> {
+        match self.is_closed {
+            true => Some(&mut self.data),
+            false => None,
         }
     }
 }
 
+impl From<Gateway> for Vec<u8> {
+    fn from(value: Gateway) -> Self {
+        value.data
+    }
+}
+
 impl AsyncWrite for Gateway {
-    fn poll_write(mut self: Pin<&mut Self>, _: &mut Context<'_>, buf: &[u8]) -> Poll<Result<usize>> {
+    fn poll_write(
+        mut self: Pin<&mut Self>,
+        _: &mut Context<'_>,
+        buf: &[u8],
+    ) -> Poll<Result<usize>> {
         if self.is_closed {
             Poll::Ready(Err(std::io::ErrorKind::BrokenPipe.into()))
         } else {
@@ -72,7 +56,13 @@ impl AsyncWrite for Gateway {
     }
 
     fn poll_shutdown(mut self: Pin<&mut Self>, _: &mut Context<'_>) -> Poll<Result<()>> {
-        self.close_write();
+        self.is_closed = true;
         Poll::Ready(Ok(()))
+    }
+}
+
+impl Default for Gateway {
+    fn default() -> Self {
+        Self::new()
     }
 }
